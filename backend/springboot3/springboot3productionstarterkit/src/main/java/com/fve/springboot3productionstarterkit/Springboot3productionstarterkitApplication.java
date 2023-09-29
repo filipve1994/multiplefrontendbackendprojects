1caa2889187d911bcbd4902df50efc9c9a2d3caa
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2023-09-29 13:25:20 +0200

    add several frontend and backend projects

---
+++ backend/springboot3/springboot3productionstarterkit/src/main/java/com/fve/springboot3productionstarterkit/Springboot3productionstarterkitApplication.java
@@ -0,0 +1,13 @@
+package com.fve.springboot3productionstarterkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3productionstarterkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3productionstarterkitApplication.class, args);
+	}
+
+}
---
+++ backend/springboot3/springboot3productionstarterkit/src/test/java/com/fve/springboot3productionstarterkit/Springboot3productionstarterkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fve.springboot3productionstarterkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3productionstarterkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
